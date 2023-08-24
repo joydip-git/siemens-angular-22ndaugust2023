7e455e9dca388a7568c1791808d581aafadc3ff7
Joydip <joydip.mondal@gmail.com>
2023-08-24 16:26:31 +0530

    Update product-list.component.ts

--- codes/day-3/pms-app/src/app/modules/products/components/product-list/product-list.component.ts
+++ codes/day-3/pms-app/src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
+
+  private psSubscription?: Subscription;
+
+  loadingComplete = false
+  errorMessage = ''
+  products?: Product[];
+
+  constructor(private ps: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.psSubscription =
+      this.ps
+        .getProducts()
+        .subscribe({
+          next: (response) => {
+            if (response.data !== null) {
+              this.products = response.data
+              this.errorMessage = ''
+              this.loadingComplete = true
+            } else {
+              this.products = undefined
+              this.errorMessage = response.message
+              this.loadingComplete = true
+            }
+          },
+          error: (err: Error) => {
+            this.products = undefined
+            this.errorMessage = err.message
+            this.loadingComplete = true
+          }
+        })
+  }
+
+  ngOnDestroy(): void {
+    this.psSubscription?.unsubscribe()
+  }
 
 }
